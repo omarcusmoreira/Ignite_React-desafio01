b5bc62d18325f0446f234a68760ba236c0949d48
Marcus Moreira <marcusmoreira@MacBook-Pro-de-Marcus.local>
2021-03-30 15:23:17 -0300

    desafio finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 import '../styles/tasklist.scss'
 
@@ -13,17 +13,43 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
-
+  
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-  }
+    
+    if(!newTaskTitle){
+      
+      alert('Campo não pode estar vazio!')
+      
 
+    }else{
+      
+      setTasks([
+        ...tasks, { id:Math.random(), title: newTaskTitle, isComplete: false }
+      ]);
+    
+    }
+    
+  }
+  
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-  }
 
+    const completedTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(completedTasks);
+  }
+  
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    
+    const filteredTaskList = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTaskList);
+
   }
 
   return (
